f4cb376faa14ce100af04fa87053c54dd2362d5d
Johnny <info@dogbytemarketing.com>
2020-05-20 02:08:09 -0400

    Quality

---
+++ src/plugin-values.js
@@ -0,0 +1,27 @@
+module.exports = pathPrefix => {
+  const imageOptions = {
+    maxWidth: 1380,
+		quality: 100,
+    wrapperStyle: ``,
+    backgroundColor: `white`,
+    linkImagesToOriginal: false,
+    showCaptions: false,
+    withWebp: true,
+    tracedSVG: false,
+    pathPrefix,
+  };
+
+  const supportedExtensions = {
+    jpeg: true,
+    jpg: true,
+    png: true,
+    webp: true,
+    tif: true,
+    tiff: true,
+  };
+
+  return {
+    imageOptions,
+    supportedExtensions,
+  };
+};
